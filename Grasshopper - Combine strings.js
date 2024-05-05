4a09d70cd2fbded37aa62ae6b40224a551a35b6b
Mark Neumann <mark.neumann.sm@gmail.com>
2024-05-05 20:33:17 +0300

    feat: Grasshopper - Combine strings.js

---
+++ Grasshopper - Combine strings.js
@@ -0,0 +1,17 @@
+/*
+Grasshopper - Combine strings
+
+DESCRIPTION:
+Combine strings function
+Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+Example:
+
+combineNames('James', 'Stevens')
+returns:
+
+'James Stevens'
+
+SOLUTION: 
+*/
+const combineNames = (...names) => names.join(' ');
