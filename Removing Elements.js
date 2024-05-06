d4441272d17f6255381ba058ddae364c8f08140c
Mark Neumann <mark.neumann.sm@gmail.com>
2024-05-06 19:44:59 +0300

    feat: Removing Elements.js

---
+++ Removing Elements.js
@@ -0,0 +1,15 @@
+/*
+Removing Elements
+
+DESCRIPTION:
+Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that!
+SOLUTION: 
+*/
+function removeEveryOther(arr){
+  return arr.filter((item,index) => index % 2 === 0)
+}
