bee5745450e617efa5b904fd51c7b4335a21c4eb
Mark Neumann <mark.neumann.sm@gmail.com>
2024-05-06 18:19:37 +0300

    feat: Even or Odd.js

---
+++ Even or Odd.js
@@ -0,0 +1,9 @@
+/*
+Even or Odd
+
+
+DESCRIPTION:
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+SOLUTION: 
+*/
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
