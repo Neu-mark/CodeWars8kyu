4db2074d7891ca488cd3c4093b5cce876208fba5
Mark Neumann <mark.neumann.sm@gmail.com>
2024-05-07 22:43:26 +0300

    feat: Remove exclamation marks.js

---
+++ Remove exclamation marks.js
@@ -0,0 +1,12 @@
+/*
+Remove exclamation marks
+
+
+DESCRIPTION:
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+SOLUTION: 
+*/
+function removeExclamationMarks(s) {
+  return s.replace(/!/g, '');
+}
