d8cfeff2e5792aa182e227753174f4a790273b5e
Mark Neumann <mark.neumann.sm@gmail.com>
2024-07-20 21:30:07 +0300

    feat: Are You Playing Banjo?.js

---
+++ Are You Playing Banjo?.js
@@ -0,0 +1,30 @@
+/*
+Are You Playing Banjo?
+
+
+DESCRIPTION:
+Create a function which answers the question "Are you playing banjo?".
+If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+name + " plays banjo" 
+name + " does not play banjo"
+Names given are always valid strings.
+
+SOLUTION: 
+*/
+function areYouPlayingBanjo(name) {
+  
+  const firstLetter = name.charAt(0).toLowerCase()
+  
+  if (firstLetter === 'r') {
+    return name + ' plays banjo'
+  }
+  
+  else {
+    return name + ' does not play banjo'
+  }
+  
+  return name;
+}
