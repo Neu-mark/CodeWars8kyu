bdac2fed18305740ea3782cc08ec7a12e4da902f
Mark Neumann <mark.neumann.sm@gmail.com>
2024-07-23 16:33:10 +0300

    feat: Opposite number.js

---
+++ Opposite number.js
@@ -0,0 +1,15 @@
+/*
+Opposite number
+
+DESCRIPTION:
+Very simple, given a number (integer / decimal / both depending on the language), find its opposite (additive inverse).
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+SOLUTION: 
+function opposite(number) {
+  return(-number);
+}
