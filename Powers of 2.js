abbad155ff01ef625f747e927f13ba88635459d4
Mark Neumann <mark.neumann.sm@gmail.com>
2024-07-26 18:36:52 +0300

    feat: Powers of 2.js

---
+++ Powers of 2.js
@@ -0,0 +1,15 @@
+/*
+Powers of 2
+
+DESCRIPTION:
+Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+Examples
+n = 0  ==> [1]        # [2^0]
+n = 1  ==> [1, 2]     # [2^0, 2^1]
+n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+SOLUTION: 
+*/
+function powersOfTwo(n){
+  return Array.from({length: n + 1}, (v, i) => 2**i);
+}
