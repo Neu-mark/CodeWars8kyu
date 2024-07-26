ddf0a84c14afa7b762a6af02e15a8c29c90926c5
Mark Neumann <mark.neumann.sm@gmail.com>
2024-07-26 18:31:16 +0300

    feat: Count Odd Numbers below n.js

---
+++ Count Odd Numbers below n.js
@@ -0,0 +1,15 @@
+/*
+Count Odd Numbers below n
+
+
+DESCRIPTION:
+Given a number n, return the number of positive odd numbers below n, EASY!
+
+Examples (Input -> Output)
+7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+15 -> 7 (because odd numbers below 15 are [1, 3, 5, 7, 9, 11, 13])
+SOLUTION: 
+*/
+function oddCount(n){
+  return Math.floor(n/2);
+}
